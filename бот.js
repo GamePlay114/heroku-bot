c7f20838c34844e03f64f25f3bd782eff00d7932
GamePlay114 <63247049+GamePlay114@users.noreply.github.com>
2020-04-06 19:01:42 +0300

    Update бот.js

--- бот.js
+++ бот.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
-var prefix = '!'
- 
- client.on('message', message => {
-    if(message.author === client.user) return;
-    if(message.content.startsWith(prefix + 'help')) {
-        message.channel.sendMessage('Привет я БОТ!');
-     }
+client.on('ready', () => {
+ console.log('I am ready!');
  });
 
-client.login('Njk2NDI1ODEzMjQyODA2Mzg2.XoovlQ.bJWfK_stvgOK3JbQqPxcVnKcX44')
+client.on('message', message => {
+ if (message.content === 'ping') {
+  message.reply('pong');
+  }
+ });
+
+// THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
